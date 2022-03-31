1c9d38285e63ae91693d070542d4c4eda7274a06
ssarmiento05 <samuelosar3@hotmail.com>
2022-03-31 09:15:14 -0600

    primeros archivos

---
+++ Proyecto_1.1.1/Form.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+<link rel="stylesheet" href="style2.css">
+</head>
+    <body>
+    <form action=" " method="post">
+        Código <input type="text" name="cod" required><br>
+        Titulo <input type="text" name="tit" required><br>
+        Autor  <input type="text" name="aut" required><br>
+        Genero <br><input type="text" name="gen" required><br>
+        Editorial <br><input type="text" name="edit" required><br>
+        Cantidad  <br><input type="number" name="can" required><br>
+        Fecha Publicación <br><input class="date" type="date" name="fpublic" required><br>
+        Edición <input type="text" name="edic" required><br>
+        <input class="button"type="submit" value="Enviar">
+</form>
+</body>
+</html>
---
+++ Proyecto_1.1.1/libros.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-03-2022 a las 15:42:11
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `libreria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `Codigo` int(20) DEFAULT NULL,
+  `Titulo` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `Autor` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `Genero` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `Editorial` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `Cantidad` int(30) NOT NULL,
+  `Fecha Publicacion` date NOT NULL,
+  `Edicion` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
